5f47cfe677054ade09b480ee0cfd77b8dbbd876b
yangsanya <zdarova4el@mail.ru>
2021-10-04 18:27:32 +0500

    added login/logout logic

--- mediumish/account/urls.py
+++ mediumish/account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import login_page, register_page, logoutUser
 
 urlpatterns = [
-
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
+    path('logout/', logoutUser, name='logout'),
 ]
