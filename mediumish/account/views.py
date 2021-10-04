61ccb5383b162ec305447613598ddae00780a378
yangsanya <zdarova4el@mail.ru>
2021-10-04 18:26:57 +0500

    added login/logout logic

--- mediumish/account/views.py
+++ mediumish/account/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def login_page(request):
+    page = 'login'
+    # if request.user.is_authenticated:
+    #     return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exists')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exists')
+    return render(request, 'account/login.html')
+
+
+def logoutUser(request):
+    logout(request)
+
+    return redirect('home')
+
+
+
+def register_page(request):
+    return render(request, 'account/register.html')
