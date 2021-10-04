0458545f63a3c16278dc0cd9cb0374ad143785e0
yangsanya <zdarova4el@mail.ru>
2021-10-04 20:36:19 +0500

    added related posts logic

--- mediumish/blog/views.py
+++ mediumish/blog/views.py
@@ -17,8 +17,14 @@ class Home(ListView):
         return context
 
 
-
 class GetPost(DetailView):
     model = Post
     template_name = 'blog/post.html'
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+
+        context['related'] = Post.objects.filter(tags__in=self.object.tags.all()).distinct()[:3]
+
+        return context
