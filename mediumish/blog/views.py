6e1ff284567dcaabd4dfd1ba1329633683f329d7
yangsanya <zdarova4el@mail.ru>
2021-10-12 16:56:51 +0500

    refactor

--- mediumish/blog/views.py
+++ mediumish/blog/views.py
@@ -39,7 +39,6 @@ class GetAuthorInfo(DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
 
-        # context['author'] = User.objects.get()
         context['author_posts'] = Post.objects.filter(author=self.get_object())
 
         return context
